c870853f54e51ec778551467adce79aaff1b14d8
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-16 13:33:41 -0700

    migration done

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,1 +1,29 @@
-const 
+const jwt = require('jsonwebtoken');
+
+const jwtKey = process.env.JWT_SECRET || 'Brooklyn99';
+
+module.exports={
+    authenticate,
+    jwtKey
+};
+
+function authenticate(req, res, next){
+    const token = req.get('Authentication');
+
+    if(token){
+        jwt.verify(token, jwtKey, (err, decoded) => {
+            if(err){
+                return res.status(400).json(
+                    console.log(err)
+                );
+                
+            }
+            req.decoded = decoded;
+                next();
+        })
+    }else{
+        return res.status(401).json({
+            message: "No token, no entry"
+        })
+    }
+}
