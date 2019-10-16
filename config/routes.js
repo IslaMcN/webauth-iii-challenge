af256f3b63a168bc2b80a22ff77d8dcb45ed4dbf
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-16 13:16:23 -0700

    authh middleware done

---
+++ config/routes.js
@@ -0,0 +1,23 @@
+server.post('/register', (req, res) => {
+
+})
+
+server.post('/login', (req, res) => {
+
+})
+
+server.get('/users', (req, res)=>{
+
+})
+
+server.get('/users/:id', (req, res) => {
+
+})
+
+server.delete('/users/:id', (req, res) => {
+
+})
+
+server.put('/users/:id', (req, res) => {
+    
+})
